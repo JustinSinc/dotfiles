cca2ae1f94578ff05217daedec5bc3dbd4bee3d2
JustinSinc <jpsinkula@hotmail.com>
2014-11-14 13:15:40 -0600

    Update attach.sh

--- Scripts/attach.sh
+++ Scripts/attach.sh
@@ -6,7 +6,7 @@ existing_sessions=$(tmux list-sessions | head -n1 | cut -d ":" -f 1)
 # if an instance doesn't exist, create one. Otherwise, attach to the existing one.
 if [ -z "$existing_sessions" ]
   then
-    st -e tmux
+    urxvt -e tmux
   else
-    st -e tmux new-session -t $existing_sessions
+    urxvt -e tmux new-session -t $existing_sessions
 fi
