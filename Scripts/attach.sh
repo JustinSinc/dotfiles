5d227db0b049a9d1e1213cae732ca2f23bd6beef
JustinSinc <jpsinkula@hotmail.com>
2014-11-26 15:32:47 -0600

    Update attach.sh

--- Scripts/attach.sh
+++ Scripts/attach.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Attach to existing tmux sessions, if any, otherwise create new one.
 
 # check for existing tmux instances
 existing_sessions=$(tmux list-sessions | head -n1 | cut -d ":" -f 1)
