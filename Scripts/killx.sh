d03ae53e868482b755a36148f93e65cbfe01e65c
JustinSinc <jpsinkula@hotmail.com>
2016-03-09 17:54:06 -0600

    Delete killx.sh

--- Scripts/killx.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Find all running X.org processes and kill them
-
-for i in `pgrep X`
-do
-        kill $i
-done
-
-# Realized that this is actually just pkill, I'm leaving it up for my own humiliation.
